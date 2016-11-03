198c2bbf9861b0593d0f6ec13345044704b6adc6
Matt Moncur <matty.moncur@gmail.com>
2016-11-03 15:19:18 -0400

    adding more tests to verify that code is working

--- L33tCodeTests/TwoSumTests.m
+++ L33tCodeTests/TwoSumTests.m
@@ -15,16 +15,6 @@
 
 @implementation TwoSumTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testThatIndicesZeroAndOneReturnedForTargetNine {
     //Array: [2, 7, 11, 15]
     NSMutableArray<NSNumber *> *myArray = [NSMutableArray new];
@@ -39,6 +29,54 @@
     
     XCTAssertEqualObjects(result[0], [NSNumber numberWithInt:0]);
     XCTAssertEqualObjects(result[1], [NSNumber numberWithInt:1]);
+}
+
+- (void)testThatIndicesOneAndTwoReturnedForTargetEighteen {
+    //Array: [2, 7, 11, 15]
+    NSMutableArray<NSNumber *> *myArray = [NSMutableArray new];
+    [myArray addObject:[NSNumber numberWithInt:2]];
+    [myArray addObject:[NSNumber numberWithInt:7]];
+    [myArray addObject:[NSNumber numberWithInt:11]];
+    [myArray addObject:[NSNumber numberWithInt:15]];
+    
+    TwoSum *twoSum = [[TwoSum alloc] init];
+    
+    NSArray *result = [twoSum findIndicesForSum:18 inArray:myArray];
+    
+    XCTAssertEqualObjects(result[0], [NSNumber numberWithInt:1]);
+    XCTAssertEqualObjects(result[1], [NSNumber numberWithInt:2]);
+}
+
+- (void)testThatIndicesOneAndThreeReturnedForTargetTwentyTwo {
+    //Array: [2, 7, 11, 15]
+    NSMutableArray<NSNumber *> *myArray = [NSMutableArray new];
+    [myArray addObject:[NSNumber numberWithInt:2]];
+    [myArray addObject:[NSNumber numberWithInt:7]];
+    [myArray addObject:[NSNumber numberWithInt:11]];
+    [myArray addObject:[NSNumber numberWithInt:15]];
+    
+    TwoSum *twoSum = [[TwoSum alloc] init];
+    
+    NSArray *result = [twoSum findIndicesForSum:22 inArray:myArray];
+    
+    XCTAssertEqualObjects(result[0], [NSNumber numberWithInt:1]);
+    XCTAssertEqualObjects(result[1], [NSNumber numberWithInt:3]);
+}
+
+- (void)testThatIndicesZeroAndThreeReturnedForTargetSeventeen {
+    //Array: [2, 7, 11, 15]
+    NSMutableArray<NSNumber *> *myArray = [NSMutableArray new];
+    [myArray addObject:[NSNumber numberWithInt:2]];
+    [myArray addObject:[NSNumber numberWithInt:7]];
+    [myArray addObject:[NSNumber numberWithInt:11]];
+    [myArray addObject:[NSNumber numberWithInt:15]];
+    
+    TwoSum *twoSum = [[TwoSum alloc] init];
+    
+    NSArray *result = [twoSum findIndicesForSum:17 inArray:myArray];
+    
+    XCTAssertEqualObjects(result[0], [NSNumber numberWithInt:0]);
+    XCTAssertEqualObjects(result[1], [NSNumber numberWithInt:3]);
 }
 
 @end
