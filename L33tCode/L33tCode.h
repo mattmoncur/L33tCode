256f148dc876e25750a96d957d439b6e2bd6bc10
Matt Moncur <matty.moncur@gmail.com>
2016-11-03 14:12:08 -0400

    initial framework structure

---
+++ L33tCode/L33tCode.h
@@ -0,0 +1,19 @@
+//
+//  L33tCode.h
+//  L33tCode
+//
+//  Created by Matt Moncur on 2016-11-03.
+//  Copyright © 2016 Moncur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for L33tCode.
+FOUNDATION_EXPORT double L33tCodeVersionNumber;
+
+//! Project version string for L33tCode.
+FOUNDATION_EXPORT const unsigned char L33tCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <L33tCode/PublicHeader.h>
+
+
