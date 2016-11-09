5a1996eb8e17beb3e90ff3d8455b72ed565079b9
Matt Moncur <matty.moncur@gmail.com>
2016-11-09 16:41:55 -0500

    * pulling vertical and horizontal traversal out to functions. * adding more comments for clarity.

--- L33tCode/Battleships/Battleships.m
+++ L33tCode/Battleships/Battleships.m
@@ -12,20 +12,23 @@
 
 NSString *const ShipIndicator = @"X";
 
+/*
+ * Traverses through the board counting unique battleships. 
+ * Battleships must have space between each other and run vertically or horizontally.
+ */
 - (int)countBattleships: (NSArray *)board {
     int count = 0;
     NSMutableArray *visitedCoordinates = [NSMutableArray array];
     
     // Traverse through the board top -> down, left -> right
-    
     for (int y = 0; y < board.count; y++) {
         NSArray *row = (NSArray *)board[y];
         for (int x = 0; x < row.count; x++) {
             
             BOOL coordinateAlreadyVisited = [visitedCoordinates containsObject: [self coordinateFormatterWithX:x andY:y]];
-            NSString *value = row[x];
-            
-            if ([value isEqualToString:ShipIndicator] && !coordinateAlreadyVisited) {
+            //If discovered a new ship, increment the count and store coordinates for battleship so unique ships are detected
+            NSString *currentValue = row[x];
+            if ([currentValue isEqualToString:ShipIndicator] && !coordinateAlreadyVisited) {
                 NSArray *battleshipCoordinates = [self findCoordinatesForBattleshipInBoard:board atX:x andY:y];
                 [visitedCoordinates addObjectsFromArray: battleshipCoordinates];
                 count ++;
@@ -37,41 +40,61 @@ NSString *const ShipIndicator = @"X";
     return count;
 }
 
-
 /*
- *  Since we know that we are traversing top -> down, left -> right do not check back. Returns visited coordinates
+ *  Finds all coordinates for battleship, starting at point x,y.
  */
 - (NSArray *) findCoordinatesForBattleshipInBoard: (NSArray *) board atX: (int) x andY: (int) y {
     NSMutableArray *battleshipCoordinates = [NSMutableArray array];
     //Record current coordinate
     [battleshipCoordinates addObject: [self coordinateFormatterWithX:x andY:y]];
     
-    //Assuming a valid board. Traverse vertical and horiztonal
-    
-    //Horizontal
-    NSArray *row = (NSArray *)board[y];
-    for (int i = x+1; i < row.count; i++) {
-        NSString *value = row[i];
-        if ([value isEqualToString:ShipIndicator]) {
-            [battleshipCoordinates addObject: [self coordinateFormatterWithX:i andY:y]];
-        }
-    }
-    
-    //Vertical - Descending
-    for (int j = y+1; j < board.count; j++) {
-        NSArray *row = (NSArray *)board[j];
-        NSString *value = row[x];
-        if ([value isEqualToString:ShipIndicator]) {
-            [battleshipCoordinates addObject: [self coordinateFormatterWithX:x andY:j]];
-        }
-    }
-    
-    //TODO: would be smart about determine where to traverse horizonal or vertical
+    //TODO: Determine whether to traverse horizonal or vertical. Don't just assume that board is valid
+    [battleshipCoordinates addObjectsFromArray: [self traverseBattleShipHorizontallyInBoard:board atX:x andY:y]];
+    [battleshipCoordinates addObjectsFromArray: [self traverseBattleShipVerticallyInBoard:board atX:x andY:y]];
     
     return battleshipCoordinates;
 }
 
 
+/*
+ * Starting at x traverses horizontally from left -> right. 
+ * Assumes that all values to the left of x have already been traversed
+ */
+- (NSArray *) traverseBattleShipHorizontallyInBoard: (NSArray *) board atX: (int) x andY: (int) y {
+    NSMutableArray *horizontalCoordinates = [NSMutableArray array];
+    
+    NSArray *row = (NSArray *)board[y];
+    for (int i = x+1; i < row.count; i++) {
+        NSString *value = row[i];
+        if ([value isEqualToString:ShipIndicator]) {
+            [horizontalCoordinates addObject: [self coordinateFormatterWithX:i andY:y]];
+        }
+    }
+    
+    return horizontalCoordinates;
+}
+
+/*
+ * Starting at y traverses vertically from top -> bottom.
+ * Assumes that all values to the above of y have already been traversed
+ */
+- (NSArray *) traverseBattleShipVerticallyInBoard: (NSArray *) board atX: (int) x andY: (int) y {
+    NSMutableArray *verticalCoordinates = [NSMutableArray array];
+    
+    for (int j = y+1; j < board.count; j++) {
+        NSArray *row = (NSArray *)board[j];
+        NSString *value = row[x];
+        if ([value isEqualToString:ShipIndicator]) {
+            [verticalCoordinates addObject: [self coordinateFormatterWithX:x andY:j]];
+        }
+    }
+    
+    return verticalCoordinates;
+}
+
+/*
+ * Common formatter for coordinates given and x and y value
+ */
 - (NSString *) coordinateFormatterWithX: (int) x andY: (int) y {
     return [NSString stringWithFormat:@"%d,%d", x, y];
 }
